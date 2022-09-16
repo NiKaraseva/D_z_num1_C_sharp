37964c302df615473f919213797c589f58f94ca9
Nika <nikarasevas@gmail.com>
2022-09-17 00:03:17 +0300

    Выполнила упражнение №1

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Программа, которая на вход принимает 2 числа и выдаёт, какое число
+большее, а какое меньшее*/
+
+Console.WriteLine("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine ($"max = {number1}, min = {number2}");
+}
+else
+{
+    Console.WriteLine ($"max = {number2}, min = {number1}");
+} 
+
