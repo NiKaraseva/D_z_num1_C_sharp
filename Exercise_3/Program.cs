8653de36ffe1596265f69a9051c4e8c703b90e12
Nika <nikarasevas@gmail.com>
2022-09-17 17:42:54 +0300

    Выполнила упражнение 3

---
+++ Exercise_3/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Программа, которая на вход принимает число и выдаёт, 
+является ли число чётным?*/
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} – чётное");
+}
+else
+{
+    Console.WriteLine($"Число {number} – не чётное");
+}
